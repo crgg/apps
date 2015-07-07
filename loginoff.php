0d2de903e833404ddcb121d9dc53fd5a72800159
crgg <crgg@sbcglobal.net>
2015-07-07 14:56:20 -0500

    CRGG APPS
    apps

---
+++ loginoff.php
@@ -0,0 +1,20 @@
+<?php
+session_destroy(); // Is Used To Destroy All Sessions
+//Or
+if(isset($_SESSION['id']))
+unset($_SESSION['id']);  //Is Used To Destroy Specified Session
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+
+</body>
+</html>
