beaf2530c368cb5dad4bd9f5f5a2e98993edb553
crgginc@gmail.com <crgginc@gmail.com>
2016-01-16 23:02:25 -0600

    make it better

--- admin/include/MenuAdmin.php
+++ admin/include/MenuAdmin.php
@@ -7,7 +7,7 @@
                         
                           <ul class="nav nav-pills">
 					   <li><a href="index.php">Home</a></li>
-					   <li><a href="categorias_list.php">Category</a></li>
+					   <li><a href="categorias_List.php">Category</a></li>
 					   <li><a href="platos_add.php">Menu</a></li>
 					   <li><a href="speciales.php">Speciales</a> </li>
 		            </ul>		 
