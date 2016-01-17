fac86cbd1c368aca80aec41b99a0facd47bd1290
crgginc@gmail.com <crgginc@gmail.com>
2016-01-16 22:02:53 -0600

    make it better

--- include/connectiondb.php
+++ include/connectiondb.php
@@ -6,8 +6,13 @@ $user = "root";
 $pass = "";
 $db = "restadb";
 
- 
-$con = mysqli_connect($serverName,$user,$pass,$db); 
+ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$con = new mysqli($serverName,$username,$password,$db); 
 
 // Check connection
 if (mysqli_connect_errno())
