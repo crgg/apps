c9ef027920ab830fd3347e11bddf412d4fee662e
crgginc@gmail.com <crgginc@gmail.com>
2016-01-16 22:14:13 -0600

    make it better

--- include/connectiondb.php
+++ include/connectiondb.php
@@ -10,7 +10,7 @@ $db = "restadb";
 db.net/heroku_562af75849a3e82?reconnect=true"));
 
 $server = $url["host"];
-$username = $url["user"];
+$username = $url["user", 'bb648ed5d5536e'];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 $con = new mysqli($serverName,$username,$password,$db); 
